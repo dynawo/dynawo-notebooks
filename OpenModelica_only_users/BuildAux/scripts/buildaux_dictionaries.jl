6fe40334dc6a2a917151d83f757a01d3d134ae88
Clara Fernandez <fernandezc@aia.es>
2026-04-28 13:27:58 +0200

    Add BuildAux support for MyPVCurrent

--- OpenModelica_only_users/BuildAux/scripts/buildaux_dictionaries.jl
+++ OpenModelica_only_users/BuildAux/scripts/buildaux_dictionaries.jl
@@ -14,6 +14,14 @@ const REPLACEMENTS = Dict{String, Any}(
         )
     ),
 
+    "Dynawo.Electrical.Photovoltaics.WECC.PVCurrentSource" => Dict(
+        "new_class" => "Dynawo.Electrical.Machines.Simplified.GeneratorPVFixed",
+        "write_modifiers" => Dict(
+            "PGen0Pu" => "P0Pu",
+            "U0Pu"    => "U0Pu",
+        )
+    ),
+
     "Dynawo.Electrical.Buses.InfiniteBusWithVariations" => Dict(
         "new_class" => "Dynawo.Electrical.Buses.InfiniteBus",
         "write_modifiers" => Dict(
@@ -122,6 +130,29 @@ const INIT_MODELS = Dict{String, Any}(
     "Dynawo.Electrical.BESS.WECC.BESSCurrentSource" => Dict(
         "init_component_suffix" => "_INIT",
         "init_class" => "Dynawo.Electrical.BESS.WECC.BESS_INIT",
+
+        "write_modifiers" => Dict(
+            "RPu"  => "RPu",
+            "XPu"  => "XPu",
+            "SNom" => "SNom",
+            "P0Pu" => "P0Pu",
+            "U0Pu" => "U0Pu",
+        ),
+
+        "extra_modifiers_raw" => [
+            "Q0Pu(fixed = false)",
+            "UPhase0(fixed = false)",
+        ],
+
+        "init_equations" => Dict(
+            "Q0Pu"    => "QGenPu",
+            "UPhase0" => "UPhase",
+        )
+    ),
+
+    "Dynawo.Electrical.Photovoltaics.WECC.PVCurrentSource" => Dict(
+        "init_component_suffix" => "_INIT",
+        "init_class" => "Dynawo.Electrical.Photovoltaics.WECC.PVCurrentSource_INIT",
 
         "write_modifiers" => Dict(
             "RPu"  => "RPu",
