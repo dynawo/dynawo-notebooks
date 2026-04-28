cfed33123f72aeb5ef6b7b0266b5e8e1e3b3b069
Clara Fernandez <fernandezc@aia.es>
2026-04-28 14:48:12 +0200

    Add Initialization support for MyPVCurrent

--- OpenModelica_only_users/Initialization/scripts/initialization_dictionaries.jl
+++ OpenModelica_only_users/Initialization/scripts/initialization_dictionaries.jl
@@ -27,6 +27,27 @@ const INIT_PARAMS = Dict{String, Any}(
         ("Q0Pu", "Q0Pu"),
     ],
 
+    "Dynawo.Electrical.Photovoltaics.WECC.PVCurrentSource" => [
+        ("Id0Pu", "Id0Pu"),
+        ("Iq0Pu", "Iq0Pu"),
+        ("PF0", "PF0"),
+        ("PInj0Pu", "PInj0Pu"),
+        ("QInj0Pu", "QInj0Pu"),
+        ("UInj0Pu", "UInj0Pu"),
+        ("UPhase0", "UPhaseInj0"),
+        ("i0Pu.re", "i0Pu.re"),
+        ("i0Pu.im", "i0Pu.im"),
+        ("iInj0Pu.re", "iInj0Pu.re"),
+        ("iInj0Pu.im", "iInj0Pu.im"),
+        ("u0Pu.re", "u0Pu.re"),
+        ("u0Pu.im", "u0Pu.im"),
+        ("uInj0Pu.re", "uInj0Pu.re"),
+        ("uInj0Pu.im", "uInj0Pu.im"),
+        ("s0Pu.re", "s0Pu.re"),
+        ("s0Pu.im", "s0Pu.im"),
+        ("Q0Pu", "Q0Pu"),
+    ],
+
     "Dynawo.Electrical.Loads.LoadPQ" => [
         ("i0Pu.re", "i0Pu.re"),
         ("i0Pu.im", "i0Pu.im"),
